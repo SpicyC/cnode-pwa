82d9d3b186f96cbbe550d77cb7c1cb9c5a89a153
pd4d10 <pd4d10@gmail.com>
2017-03-27 23:50:51 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,1 @@
-// https://stackoverflow.com/questions/33704791/how-do-i-uninstall-a-service-worker
-navigator.serviceWorker.getRegistrations().then(function (registrations) {
-  for (var i = 0; i < registrations.length; i++) {
-    registrations[i].unregister()
-  }
-})
+
