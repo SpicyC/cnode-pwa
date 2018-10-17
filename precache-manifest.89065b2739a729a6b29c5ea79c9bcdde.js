2bcbbef38c4e4afe26c640becfa8d9f857696c1d
Rongjian Zhang <pd4d10@gmail.com>
2018-10-17 16:55:35 +0000

    Deploy pd4d10/cnode-pwa to github.com/pd4d10/cnode-pwa.git:gh-pages

---
+++ precache-manifest.89065b2739a729a6b29c5ea79c9bcdde.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cce3408a7cc965d13b32d745a1110387",
+    "url": "/static/media/logo.cce3408a.svg"
+  },
+  {
+    "revision": "48f4b8b6fba484d9b8bf08b51d560402",
+    "url": "/static/media/cnodejs.48f4b8b6.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b84d3dfb3a78c1c85633",
+    "url": "/static/js/main.b84d3dfb.chunk.js"
+  },
+  {
+    "revision": "3ffccb6e67d335bcd52b",
+    "url": "/static/js/1.3ffccb6e.chunk.js"
+  },
+  {
+    "revision": "b84d3dfb3a78c1c85633",
+    "url": "/static/css/main.c4f16577.chunk.css"
+  },
+  {
+    "revision": "3ffccb6e67d335bcd52b",
+    "url": "/static/css/1.e8953f7e.chunk.css"
+  },
+  {
+    "revision": "50868429789d8977b6b958fd2cc41afc",
+    "url": "/index.html"
+  }
+];
