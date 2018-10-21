5fe0bc6d5eead58e5f5a816502729226565214a0
Rongjian Zhang <pd4d10@gmail.com>
2018-10-21 12:09:31 +0000

    Deploy pd4d10/cnode-pwa to github.com/pd4d10/cnode-pwa.git:gh-pages

---
+++ precache-manifest.44239acea2baa77b20b3367740b8ef81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "48f4b8b6fba484d9b8bf08b51d560402",
+    "url": "/static/media/cnodejs.48f4b8b6.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "caeab10a806eab9232b0",
+    "url": "/static/js/main.caeab10a.chunk.js"
+  },
+  {
+    "revision": "2825b848ca8da7ad65cd",
+    "url": "/static/js/1.2825b848.chunk.js"
+  },
+  {
+    "revision": "caeab10a806eab9232b0",
+    "url": "/static/css/main.8c5b5e40.chunk.css"
+  },
+  {
+    "revision": "2825b848ca8da7ad65cd",
+    "url": "/static/css/1.d4d22f8f.chunk.css"
+  },
+  {
+    "revision": "b23fc2eb711f23e26335a81568e49965",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4ccb36d850c64ff9a6b107660dd0866.js"
+  "/precache-manifest.44239acea2baa77b20b3367740b8ef81.js"
 );
 
 workbox.clientsClaim();
